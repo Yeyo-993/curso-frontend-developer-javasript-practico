8c274bdcdc7a7c744bb59801587f3893c7ceaa8e
Sergio Reinoso <slrs993@gmail.com>
2022-11-07 22:08:16 -0500

    Fusion del menu en mobile

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktoMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu= document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
 
 function toggleDesktopMenu () {
     desktoMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
 }
